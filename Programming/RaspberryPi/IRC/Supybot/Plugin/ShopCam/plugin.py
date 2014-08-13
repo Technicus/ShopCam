5474bbb3d3c9515e789e9bbe97e6973ec9f0197a
Technicus Spiriticus <fgardner@madisoncollege.edu>
2014-08-13 00:33:53 -0500

    Navigation works!!!

--- Programming/RaspberryPi/IRC/Supybot/Plugin/ShopCam/plugin.py
+++ Programming/RaspberryPi/IRC/Supybot/Plugin/ShopCam/plugin.py
@@ -54,10 +54,44 @@ class ShopCam(callbacks.Plugin):
     # This is the address we setup in the Arduino Program
     address = 0x04
     
-    pan = 0
-    tilt = 0
-    panCenter = 63
-    tiltCenter = 191
+    positionX = 0
+    positionY = 90
+    positionXCenter = 63
+    positionYCenter = 191
+    maxX = 90
+    minX = -90
+    maxY = 90
+    minY = 0
+    maxI2CX = 127
+    minI2CX = 1
+    maxI2CY = 128
+    minI2CY = 255
+    incrumentY = (90 / 4)
+    incrumentX = (180 / 8)
+    
+    def mapRangeX(self, X0):
+        if X0 < self.minX:
+           X0 = self.minX
+        if X0 > self.maxX:
+           X0 = self.maxX
+        #a1 = minX 
+        #a2 = maxX
+        #b1 = minI2CX
+        #b2 = maxI2CX
+        X1 = self.minI2CX + ((X0 - self.minX) * (self.maxI2CX - self.minI2CX) / (self.maxX - self.minX))
+        return X1
+     
+    def mapRangeY(self, Y0):
+        if Y0 < self.minY:
+           Y0 = self.minY
+        if Y0 > self.maxY:
+           Y0 = self.maxY
+        #a1 = minY 
+        #a2 = maxY
+        #b1 = minI2CY
+        #b2 = maxI2CY
+        Y1 = self.minI2CY + ((Y0 - self.minY) * (self.maxI2CY - self.minI2CY) / (self.maxY - self.minY))
+        return Y1
 
     def writeNumber(self, value):
         self.bus.write_byte(self.address, value)
@@ -70,60 +104,136 @@ class ShopCam(callbacks.Plugin):
         # number = bus.read_byte_data(address, 1)
         return number
 
-    def move(self, irc, msg, args, var):
-        """<byte>
-        Sends a byte (an integer from 1 to 255) to the arduino."""
-        # number = 0
-        if var < 128
-            pan = var
+    #def move(self, irc, msg, args, var):
+        #"""<byte>
+        #Sends a byte (an integer from 1 to 255) to the arduino."""
+        ## number = 0
+        #if var < 128:
+            #pan = var
+        #else:
+            #tilt = var
+        #self.writeNumber(var)
+        #time.sleep(1)
+        #number = self.readNumber()
+        #irc.reply('You moved the camera %d' % var)
+        ##irc.reply('The Arduino answered: %d' % number)
+    #move = wrap(move, ['int'])
+    
+    def move(self, irc, msg, args, X0, Y0):
+        """< pan > < tilt >
+        HeckBot expects integers from -90 to 90."""
+        X1 = self.mapRangeX(X0)
+        Y1 = self.mapRangeY(Y0)
+        irc.reply('The result for X = ( %d )' %X1)
+        irc.reply('The result for Y = ( %d )' %Y1)
+        self.writeNumber(X1)
+        #time.sleep(1)
+        self.writeNumber(Y1)
+        #time.sleep(1)
+        irc.reply('You moved the camera to ( %d %d )' %(X1, Y1))
+        self.positionX = X0
+        self.positionY = Y0
+    move = wrap(move, ['int','int'])
+
+    def pan(self, irc, msg, args, X0):
+        """< pan >
+        HeckBot expects integers from -90 to 90."""
+        X1 = self.mapRangeX(X0)
+        irc.reply('The result for X = ( %d )' %X1)
+        self.writeNumber(X1)
+        irc.reply('You panned the camera to ( %d )' %X1)
+        self.positionX = X0
+    pan = wrap(pan, ['int'])
+    
+    def tilt(self, irc, msg, args, Y0):
+        """< pan >
+        HeckBot expects integers from -90 to 90."""
+        Y1 = self.mapRangeY(Y0)
+        irc.reply('The result for Y = ( %d )' %Y1)
+        self.writeNumber(Y1)
+        irc.reply('You tilted the camera to ( %d )' %Y1)
+        self.positionY = Y0
+    tilt = wrap(tilt, ['int'])
+
+    def down(self, irc, msg, args, var):
+        """< steps >
+        HeckBot expects integers."""
+        if (self.positionY <= (self.maxY - self.incrumentY)):
+           self.positionY = self.positionY + self.incrumentY
+           if self.positionY > self.maxY:
+              self.positionY = self.maxY
+           self.writeNumber(self.mapRangeY(self.positionY))
+           irc.reply('You tilted the camera up to ( %d )' %self.positionY)
+        elif ((self.positionY >= (self.maxY - self.incrumentY)) and (self.positionY < self.maxY)):
+           if self.positionY > self.maxY:
+              self.positionY = self.maxY
+           self.writeNumber(self.mapRangeY(self.maxY))
+           self.positionY = self.maxY
+           irc.reply('You tilted the camera to the MAXIMUM!!!! ( %d )' %self.positionY)
         else:
-            tilt = var
-        self.writeNumber(var)
-        time.sleep(1)
-        number = self.readNumber()
-        irc.reply('You moved the camera %d' % var)
-        #irc.reply('The Arduino answered: %d' % number)
-    move = wrap(move, ['int'])
+           irc.reply('The camera is at its limit ( %d )' %self.positionY)
+        irc.reply('The Y position = ( %d )' %self.positionY)
+    down = wrap(down, [optional('int')])
     
     def up(self, irc, msg, args, var):
-        """<byte>
-        Sends a byte (an integer from 1 to 255) to the arduino."""
-        # number = 0
-        self.writeNumber(var)
-        time.sleep(1)
-        number = self.readNumber()
-        irc.reply('The Arduino answered: %d' % number)
-    up = wrap(up, ['int'])
-    
-    def down(self, irc, msg, args, var):
-        """<byte>
-        Sends a byte (an integer from 1 to 255) to the arduino."""
-        # number = 0
-        self.writeNumber(var)
-        time.sleep(1)
-        number = self.readNumber()
-        irc.reply('The Arduino answered: %d' % number)
-    down = wrap(down, ['int'])
+        """< steps >
+        HeckBot expects integers."""
+        if (self.positionY >= (self.minY - self.incrumentY)):
+           self.positionY = self.positionY - self.incrumentY
+           if self.positionY < self.minY:
+              self.positionY = self.minY
+           self.writeNumber(self.mapRangeY(self.positionY))
+           irc.reply('You tilted the camera up to ( %d )' %self.positionY)
+        elif ((self.positionY >= (self.minY - self.incrumentY)) and (self.positionY > self.minY)):
+           if self.positionY < self.minY:
+              self.positionY = self.minY
+           self.writeNumber(self.mapRangeY(self.minY))
+           self.positionY = self.minY
+           irc.reply('You tilted the camera to the MINIMUM!!!! ( %d )' %self.positionY)
+        else:
+           irc.reply('The camera is at its limit ( %d )' %self.positionY)
+        irc.reply('The Y position = ( %d )' %self.positionY)
+    up = wrap(up, [optional('int')])
     
     def left(self, irc, msg, args, var):
-        """<byte>
-        Sends a byte (an integer from 1 to 255) to the arduino."""
-        # number = 0
-        self.writeNumber(var)
-        time.sleep(1)
-        number = self.readNumber()
-        irc.reply('The Arduino answered: %d' % number)
-    left = wrap(left, ['int'])
+        """< steps >
+        HeckBot expects integers."""
+        if (self.positionX <= (self.maxX - self.incrumentX)):
+           self.positionX = self.positionX + self.incrumentX
+           if self.positionX > self.maxX:
+              self.positionX = self.maxX
+           self.writeNumber(self.mapRangeX(self.positionX))
+           irc.reply('You tilted the camera up to ( %d )' %self.positionX)
+        elif ((self.positionX >= (self.maxX - self.incrumentX)) and (self.positionX < self.maxX)):
+           if self.positionX > self.maxX:
+              self.positionX = self.maxX
+           self.writeNumber(self.mapRangeX(self.maxX))
+           self.positionX = self.maxX
+           irc.reply('You tilted the camera to the MAXIMUM!!!! ( %d )' %self.positionX)
+        else:
+           irc.reply('The camera is at its limit ( %d )' %self.positionX)
+        irc.reply('The X position = ( %d )' %self.positionX)
+    left = wrap(left, [optional('int')])
     
     def right(self, irc, msg, args, var):
-        """<byte>
-        Sends a byte (an integer from 1 to 255) to the arduino."""
-        # number = 0
-        self.writeNumber(var)
-        time.sleep(1)
-        number = self.readNumber()
-        irc.reply('The Arduino answered: %d' % number)
-    right = wrap(right, ['int'])
+        """< steps >
+        HeckBot expects integers."""
+        if (self.positionX >= (self.minX - self.incrumentX)):
+           self.positionX = self.positionX - self.incrumentX
+           if self.positionX < self.minX:
+              self.positionX = self.minX
+           self.writeNumber(self.mapRangeX(self.positionX))
+           irc.reply('You tilted the camera up to ( %d )' %self.positionX)
+        elif ((self.positionX >= (self.minX - self.incrumentX)) and (self.positionX > self.minX)):
+           if self.positionX < self.minX:
+              self.positionX = self.minX
+           self.writeNumber(self.mapRangeX(self.minX))
+           self.positionX = self.minX
+           irc.reply('You tilted the camera to the MINIMUM!!!! ( %d )' %self.positionX)
+        else:
+           irc.reply('The camera is at its limit ( %d )' %self.positionX)
+        irc.reply('The X position = ( %d )' %self.positionX)
+    right = wrap(right, [optional('int')])
 
 Class = ShopCam
 
